c79ac37e823d4a8554beca1f42b889533fdcc7f5
Christian Jácome <129555977+ChrisJMora@users.noreply.github.com>
2024-05-06 18:15:21 -0500

    Se corrige la libreria de movimiento

--- Assets/Scripts/World/Entities/Mobs/Behaviour/Move/MovController.cs
+++ Assets/Scripts/World/Entities/Mobs/Behaviour/Move/MovController.cs
@@ -21,14 +21,7 @@ namespace Scripts.World.Entities.Mobs.Behaviour.Move
         {
             float xInput = Input.GetAxisRaw("Horizontal");
             float zInput = Input.GetAxisRaw("Vertical");
-
-            if (movUtils.CheckWall(movUtils.targetPosition))
-                movUtils.Jump(transform, speed);
-            else
-            if (!movUtils.CheckWall(movUtils.targetPosition + Vector3.down))
-                movUtils.Fall(transform, speed);
-            if (!movUtils.isJumping && !movUtils.isFalling)
-                movUtils.Move(transform, distance, speed, xInput, zInput);
+            movUtils.Move(transform, distance, speed, zInput, xInput);
         }
     }
 }
--- Assets/Scripts/World/Entities/Mobs/Behaviour/Move/MovUtilities.cs
+++ Assets/Scripts/World/Entities/Mobs/Behaviour/Move/MovUtilities.cs
@@ -1,101 +1,154 @@
-using UnityEngine;
 using Scripts.CustomAttributes;
+using UnityEngine;
 
 namespace Scripts.World.Entities.Mobs.Behaviour.Move
 {
     [System.Serializable]
     public class MovUtilities
     {
-        [Header("Movement Properties")]
-        [ReadOnly] public Vector3 direction;
+        private const float JumpHeight = 1f;
+        private const float MinDistance = 0.05f;
 
-        [Header("Referencial Points")]
+        private Vector3 direction;
         [ReadOnly] public Vector3 currentPosition;
         [ReadOnly] public Vector3 targetPosition;
         [ReadOnly] public Vector3 destinyPosition;
-        [ReadOnly] public float currentHeight;
+        private float currentHeight;
 
-        [Header("States")]
-        [ReadOnly] public bool ReachDestination;
-        [ReadOnly] public bool isJumping;
-        [ReadOnly] public bool isFalling;
-
-        public Vector3 CurrentPosition => targetPosition - direction;
+        [ReadOnly] public bool ReachDestination = true;
+        private bool isJumping;
+        private bool isFalling;
 
         public MovUtilities(Vector3 origin)
         {
-            currentPosition = destinyPosition = origin;
+            currentPosition = targetPosition = destinyPosition = origin;
             currentHeight = 0;
         }
 
         public void Move(Transform entity, float distance, float speed, float xInput, float zInput)
         {
-            UpdateEntityPosition(entity, speed, targetPosition); // Update the entity position
-            if (Vector3.Distance(currentPosition, targetPosition) <= .05f && !ReachDestination)
+            if (CheckWall(targetPosition))
+            {
+                Jump(entity, speed);
+            }
+            else if (!CheckWall(targetPosition + Vector3.down))
+            {
+                Fall(entity, speed);
+            }
+            if (!isJumping && !isFalling)
+            {
+                UpdatePositions(entity, distance, speed, xInput, zInput);
+            }
+        }
+
+        private void UpdatePositions(Transform entity, float distance, float speed, float xInput, float zInput)
+        {
+            // Check if the entity has reached the target position
+            if (Vector3.Distance(currentPosition, targetPosition) <= MinDistance && !ReachDestination)
+            {
                 targetPosition += direction;
-            if (Vector3.Distance(targetPosition, destinyPosition) <= .05f)
+                Debug.Log("Target position updated to: " + targetPosition);
+            }
+
+            // Update the position of the entity
+            UpdateEntityPosition(entity, speed, targetPosition);
+
+            // Check if the entity has reached the destiny position
+            if (Vector3.Distance(currentPosition, destinyPosition) <= MinDistance)
+            {
                 ReachDestination = true;
-            if (xInput == 0 && zInput == 0) return; // Dont move if there is no input
-            direction = SetUpDirection(entity, xInput, zInput);
-            if (ReachDestination) {
+            }
+
+            if (xInput == 0 && zInput == 0)
+            {
+                return; // No input, no movement
+            }
+
+            if (ReachDestination)
+            {
+                Debug.Log("Moving to the next destination");
+                direction = SetUpDirection(entity, xInput, zInput);
                 destinyPosition += direction * distance;
                 ReachDestination = false;
             }
         }
-        public void Jump(Transform entity, float speed)
+
+        private void Jump(Transform entity, float speed)
         {
             isJumping = true;
             UpdateEntityPosition(entity, speed, currentPosition + Vector3.up);
-            if (entity.position.y >= currentHeight + 1f)
+
+            if (entity.position.y >= currentHeight + JumpHeight)
             {
-                currentHeight += 1;
-                targetPosition += Vector3.up;
-                destinyPosition += Vector3.up;
-                isJumping = false;
+                FinishJump();
             }
         }
-        public void Fall(Transform entity, float speed)
+
+        private void FinishJump()
+        {
+            currentHeight += JumpHeight;
+            targetPosition += Vector3.up;
+            destinyPosition += Vector3.up;
+            isJumping = false;
+        }
+
+        private void Fall(Transform entity, float speed)
         {
             isFalling = true;
             UpdateEntityPosition(entity, speed, targetPosition);
-            if (Vector3.Distance(entity.position, targetPosition) <= .05f)
+
+            if (Vector3.Distance(entity.position, targetPosition) <= MinDistance)
             {
-                currentHeight -= 1;
-                targetPosition += Vector3.down;
-                destinyPosition += Vector3.down;
-                isFalling = false;
+                FinishFall();
             }
         }
-        public bool CheckWall(Vector3 target)
+
+        private void FinishFall()
         {
-            return Physics.CheckSphere(target, .05f, LayerMask.GetMask("Wall"));
+            currentHeight -= JumpHeight;
+            targetPosition += Vector3.down;
+            destinyPosition += Vector3.down;
+            isFalling = false;
         }
+
+        public void Stop()
+        {
+            destinyPosition = targetPosition;
+        }
+
+        private bool CheckWall(Vector3 target)
+        {
+            return Physics.CheckSphere(target, MinDistance, LayerMask.GetMask("Wall"));
+        }
+
         private void UpdateEntityPosition(Transform entity, float speed, Vector3 targetPosition)
         {
             currentPosition = entity.position;
             entity.position = Vector3.MoveTowards(currentPosition, targetPosition, speed * Time.deltaTime);
         }
+
         private Vector3 SetUpDirection(Transform entity, float x, float z)
         {
-            if (Mathf.Abs(x) == 1f) return MoveSideWays(entity, x);
-            else
-            if (Mathf.Abs(z) == 1f) return MoveForward(entity, z);
+            if (Mathf.Abs(x) == 1f)
+            {
+                return SetUpHorizontalRotation(entity, x);
+            }
+            else if (Mathf.Abs(z) == 1f)
+            {
+                return SetUpVerticalRotation(entity, z);
+            }
             return Vector3.zero;
         }
-        private Vector3 MoveSideWays(Transform entity, float orientation)
+
+        private Vector3 SetUpHorizontalRotation(Transform entity, float orientation)
         {
-            if (orientation == 1f)
-                entity.rotation = Quaternion.Euler(0f, 90f, 0f);
-            else
-                entity.rotation = Quaternion.Euler(0f, -90f, 0f);
+            entity.rotation = Quaternion.Euler(0f, orientation * 90f, 0f);
             return new Vector3(0f, 0f, orientation);
         }
-        private Vector3 MoveForward(Transform entity, float orientation)
+
+        private Vector3 SetUpVerticalRotation(Transform entity, float orientation)
         {
-            if (orientation == 1f)
-                entity.rotation = Quaternion.Euler(0f, 0f, 0f);
-            else
-                entity.rotation = Quaternion.Euler(0f, 180f, 0f);
+            entity.rotation = Quaternion.Euler(0f, orientation == 1f ? 0f : 180f, 0f);
             return new Vector3(orientation, 0f, 0f);
         }
     }
